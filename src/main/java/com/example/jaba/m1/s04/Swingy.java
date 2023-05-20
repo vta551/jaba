16ded3539654fa7448130ec8b4ae42904df35ad0
Emanuele <egalli64@gmail.com>
2023-05-20 16:12:52 +0200

    swing hello

---
+++ src/main/java/com/example/jaba/m1/s04/Swingy.java
@@ -0,0 +1,32 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m1.s04;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import static javax.swing.SwingUtilities.invokeLater;
+
+/**
+ * A friendlier "hello"
+ */
+public class Swingy {
+    public static void main(String[] args) {
+        String message = "Hello, " + (args.length == 0 ? "stranger" : args[0]);
+        invokeLater(() -> {
+            JFrame frame = new JFrame("Hello!");
+            frame.setSize(400, 100);
+            frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JPanel panel = new JPanel();
+            panel.add(new JLabel(message));
+            frame.add(panel);
+            frame.setVisible(true);
+        });
+    }
+}
