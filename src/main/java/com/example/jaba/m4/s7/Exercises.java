d8c76ca5e8581f793d389dadf0f05bd252858ca8
Emanuele <egalli64@gmail.com>
2023-09-20 10:37:05 +0200

    loop while/do-while exercises

---
+++ src/main/java/com/example/jaba/m4/s7/Exercises.java
@@ -0,0 +1,27 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m4.s7;
+
+/**
+ * Exercises on looping with while / do-while
+ */
+public class Exercises {
+    public static void main(String[] args) {
+        int x = 64;
+
+        System.out.println("The initial value is " + x);
+
+        // TODO: (a1) While x divided by 2 is even, print it
+
+        // TODO: (a2) While x is even, and print it and divide it by 2
+
+        // TODO: (a3) Print x and divide it by 2, until the result is even
+
+        // TODO: (b) Calculate and print the sum of the values in [1..x]
+        // use a while or do-while loop!
+        System.out.println("Gauss summation formula: " + (x * (x + 1) / 2));
+    }
+}
