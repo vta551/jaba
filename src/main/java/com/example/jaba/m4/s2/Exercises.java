41b94c4c3a2190af562a44ee99a5339722d0fa07
Emanuele <egalli64@gmail.com>
2023-09-20 09:24:41 +0200

    exercise

---
+++ src/main/java/com/example/jaba/m4/s2/Exercises.java
@@ -0,0 +1,28 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m4.s2;
+
+/**
+ * Branch in code execution by checking conditions
+ */
+public class Exercises {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 42;
+        int c = 23;
+
+        System.out.println("The sum of the three variables is " + (a + b + c));
+
+        // TODO: compare a, b, and c to print the largest value
+        System.out.println("The largest value is ... ");
+
+        // TODO: compare a, b, and c to print ONE of the two alternative strings:
+        System.out.println("[The values are not repeated / A value is repeated ... ]");
+
+        // TODO: compare a, b, and c to print ONE of the three alternative strings:
+        System.out.println("The value in a is [greater than / less than / equal to] b");
+    }
+}
