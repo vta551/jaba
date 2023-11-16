160261a9c6d920f63ed13398b05118402c4b1671
Emanuele <egalli64@gmail.com>
2023-11-16 11:28:01 +0100

    moved to own package

--- src/main/java/com/example/jaba/m2/s2/ex/Exercise1.java
+++ src/main/java/com/example/jaba/m2/s2/ex/Exercise1.java
@@ -3,12 +3,12 @@
  * 
  * https://github.com/egalli64/jaba
  */
-package com.example.jaba.m2.s2;
+package com.example.jaba.m2.s2.ex;
 
 /**
  * A very simple exercise in three steps on array
  */
-public class Exercise {
+public class Exercise1 {
     /**
      * <li>(1) create an array with "one", "two", "three", "four" in it
      * <li>(2) print the number of its elements
