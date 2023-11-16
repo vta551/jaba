57e693d1ba8faa27a806341df5b7671762c68b6c
Emanuele <egalli64@gmail.com>
2023-11-16 11:32:22 +0100

    another exercise

---
+++ src/main/java/com/example/jaba/m2/s2/ex/Exercise2.java
@@ -0,0 +1,24 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m2.s2.ex;
+
+/**
+ * Another very simple exercise on array
+ */
+public class Exercise2 {
+    /**
+     * <li>(1) define an array of char where the elements represent your name
+     * <li>(2) print the array length
+     * <li>(3) print the first and the last character in it
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        // TODO
+
+        System.out.println("Done");
+    }
+}
